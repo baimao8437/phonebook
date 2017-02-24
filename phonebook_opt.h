7878002679e44b63421f3aacd515aaaccbac7b1b
baimao8437 <baimao8437@gmail.com>
2017-02-24 23:28:03 +0800

    Move other detail into a new structure

--- phonebook_opt.h
+++ phonebook_opt.h
@@ -6,8 +6,21 @@
 /* TODO: After modifying the original version, uncomment the following
  * line to set OPT properly */
 #define OPT 1
+typedef struct __PHONE_BOOK_DETAIL {
+    char firstName[16];
+    char email[16];
+    char phone[10];
+    char cell[10];
+    char addr1[16];
+    char addr2[16];
+    char city[16];
+    char state[2];
+    char zip[5];
+} detail;
+
 typedef struct __PHONE_BOOK_ENTRY {
     char lastName[MAX_LAST_NAME_SIZE];
+    struct __PHONE_BOOK_DETAIL *details;
     struct __PHONE_BOOK_ENTRY *pNext;
 } entry;
 
